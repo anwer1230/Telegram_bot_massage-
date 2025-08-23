6264f02e2eb22ead49c0d5c9c5d998c95620af5a
anwer1230 <anwrfwad178@gmail.com>
2025-08-23 20:39:22 +0300

    ترتيب الملفات وإضافة ملفات المشروع الأساسية

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from jinja2 import Environment, FileSystemLoader
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+env = Environment(loader=FileSystemLoader("templates"))
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    template = env.get_template("index.html")
+    return template.render(request=request)
